8b65390e0b3573141f09446b426b5f3bd187886b
sue rose <suehaizoglu@gmail.com>
2023-07-19 15:17:21 +0100

    added random winning colour logic

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
-let colourNum = "";
-colourChange();
+let colourNum = returnRandomColour();
+console.log(colourNum);
+setColourForBlocks();
 
 //switching to easy mode
-document.querySelector(".easyMode").addEventListener("click", switchToEasyMode);
+// document.querySelector(".easyMode").addEventListener("click", switchToEasyMode);
 
-function switchToEasyMode() {
-  document.querySelector(".colour4").style.display = "none";
-  document.querySelector(".colour5").style.display = "none";
-  document.querySelector(".colour6").style.display = "none";
-}
+// function switchToEasyMode() {
+//   document.querySelector(".colour4").style.display = "none";
+//   document.querySelector(".colour5").style.display = "none";
+//   document.querySelector(".colour6").style.display = "none";
+// }
 
 //switching to hard mode
-document.querySelector(".hardMode").addEventListener("click", switchToHardMode);
+// document.querySelector(".hardMode").addEventListener("click", switchToHardMode);
 
-function switchToHardMode() {
-  document.querySelector(".colour4").style.display = "block";
-  document.querySelector(".colour5").style.display = "block";
-  document.querySelector(".colour6").style.display = "block";
-}
+// function switchToHardMode() {
+//   document.querySelector(".colour4").style.display = "block";
+//   document.querySelector(".colour5").style.display = "block";
+//   document.querySelector(".colour6").style.display = "block";
+// }
 
-//clicking reset button
+// clicking reset button
 document
   .querySelector(".resetButton")
   .addEventListener("click", resetButtonClick);
 
 function resetButtonClick() {
-  return colourChange();
+  return setColourForBlocks();
 }
 
-function colourChange() {
-  document.querySelector(".colour1").style.backgroundColor =
-    returnRandomColour();
-  document.querySelector(".colour2").style.backgroundColor =
-    returnRandomColour();
-  document.querySelector(".colour3").style.backgroundColor =
-    returnRandomColour();
-  document.querySelector(".colour4").style.backgroundColor =
-    returnRandomColour();
-  document.querySelector(".colour5").style.backgroundColor =
-    returnRandomColour();
-  document.querySelector(".colour6").style.backgroundColor =
-    returnRandomColour();
+function setColourForBlocks() {
+colourNum = returnRandomColour();
+const winningBlockNumber = Math.trunc(Math.random()*6+1);
+console.log(winningBlockNumber);
+document.querySelector(".RGBNumber").textContent = colourNum;
+document.querySelector(`.colour${winningBlockNumber}`).style.backgroundColor = colourNum;
+  for(let i = 1; i <= 6; i++) {
+    if(i === winningBlockNumber) {
+      continue;
+    } else {
+      document.querySelector(`.colour${i}`).style.backgroundColor=returnRandomColour();
+    }
+  }
+
 }
+
 
 function returnRandomColour() {
   const randomNumbers = [];
@@ -49,28 +52,8 @@ function returnRandomColour() {
     randomNumbers.push(Math.floor(Math.random() * 256));
   }
   const result = `RGB(${randomNumbers.join(",")})`;
-  colourNum = result;
-  document.querySelector(".RGBNumber").textContent = colourNum;
   return result;
 }
 
-
-// continue working on this tomorrow
-// https://www.quora.com/How-do-you-randomize-the-position-of-divs-JavaScript-arrays-sorting-random-and-development
-//use above url for source on how to randomize the divs
-function randomizeColours() {
-  // using fisher gates shuffle method
-  // const shuffleArray = array => {
-  //     for (let i = array.length - 1; i > 0; i--) {
-  //       const j = Math.floor(Math.random() * (i + 1));
-  //       const temp = array[i];
-  //       array[i] = array[j];
-  //       array[j] = temp;
-  //     }
-  //   }
-}
-
-//find a way to randomize where the winning square colour is displayed
-//remember you do not need to randomize all of them only swapping
-//the winning square with one other square placement is enough
+// handle switching to easymode and hardmode again for new code logic
 //make sure to implement wrong and correct guesses (winning-losing clicks, guesses)
